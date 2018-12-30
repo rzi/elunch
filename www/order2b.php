446b3d30869207ca9271c33f1f75291781ba9f47
rzi <rzi@vp.pl>
2018-12-31 00:05:52 +0100

    ....

--- www/order2b.php
+++ www/order2b.php
@@ -7,12 +7,7 @@
     <link rel="stylesheet" href="/www/css/style.css" type="text/css" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-
-
-	<!-- <script src="https://code.jquery.com/jquery-1.12.4.js"
-  integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
-  crossorigin="anonymous"></script> -->
-	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
     <script type="text/javascript" src="/www/js/skrypt3.js"></script>
