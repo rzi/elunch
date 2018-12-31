8b8fa24b89a0b231a607247b34191f11a613c12b
rzi <rzi@vp.pl>
2018-12-31 16:23:53 +0100

    poprawka raport1 31/12 16.23

--- www/raport1.php
+++ www/raport1.php
@@ -7,10 +7,8 @@
 <link rel="stylesheet" href="/www/css/style.css" type="text/css" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<!--
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
--->
 <script src="/www/js/skrypt3.js"></script>
 </head>
 <body>
